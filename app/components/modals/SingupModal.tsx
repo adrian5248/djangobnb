5456d0ea263dbfc5e2a3f53527e867d20ff173b3
Adrian <adrian.duran.matias@gmail.com>
2025-04-26 16:19:19 -0600

    Sing up

--- app/components/modals/SingupModal.tsx
+++ app/components/modals/SingupModal.tsx
@@ -3,32 +3,84 @@
 import Modal from "./Modal";
 
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import useSingupModal from "@/app/hooks/useLoginModal";
 import CustomButton from "../forms/CustomButton";
+import { error } from "console";
+import apiService from "@/app/services/apiService";
+import { sign } from "crypto";
+import { handleLogin } from "@/app/lib/actions";
 
 const SingupModal = () => {
 
-    const singupModal = useSingupModal()
- 
+    //
+    // Variables
+    const router = useRouter();
+    const singupModal = useSingupModal();
+    const [email, setEmail] = useState('');
+    const [password1, setPassword1] = useState('');
+    const [password2, setPassword2] = useState('');
+    const [erros, setErrors] = useState<string[]>([]);
+
+    //
+    // Submit functionality
+
+    const SubmitSingup = async () => {
+        const formData = {
+            name: 'stein',
+            email: email,
+            password1: password1,
+            password2: password2
+        }
+
+        const response = await apiService.post('/api/auth/register/', JSON.stringify(formData));
+
+        if (response.access) {
+            
+            handleLogin(response.user.pk, response.access, response.refresh);
+
+
+            singupModal.close();
+
+            router.push('/')
+        } else {
+            const tmpErrors: string[] = Object.values(response).map((error: any) => {
+                return error;
+            })
+
+            setErrors(tmpErrors)
+        }
+    }
+
     const content = (
             <>
             
+                <form
+                    action={SubmitSingup}
+                    className="space-y-4"
+                >
+                    <input onChange={(e) => setEmail(e.target.value)} placeholder="Your e-mail address" type="email" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl"/>
 
-                <form className="space-y-4">
-                    <input placeholder="Your e-mail address" type="email" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl"/>
+                    <input onChange={(e) => setPassword1(e.target.value)} placeholder="Your password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl"/>
 
-                    <input placeholder="Your password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl"/>
+                    <input onChange={(e) => setPassword2(e.target.value)} placeholder="Repeat password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl"/>
 
-                    <input placeholder="Repeat password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl"/>
+                    {erros.map((error, index) => {
+                        return (
+                            <div
+                                key={`error_${index}`}
+                                className="p-5 bg-airbnb text-white rounded-xl opacity-80"
+                            >
+                                {error}
+                            </div>
+                        )
+                    })}
 
-                    <div className="p-5 bg-airbnb text-white rounded-xl opacity-80">
-                        The error message
-                    </div>
 
                     <CustomButton
                     
                         label="Submit"
-                        onClick={() => console.log('Test')}
+                        onClick={SubmitSingup}
                     
                     />
                 </form>
--- app/services/apiService.ts
+++ app/services/apiService.ts
@@ -25,6 +25,26 @@ const apiService = {
                     reject(error);
                 }))
             })
+    },
+
+    post: async function(url: string, data: any): Promise<any> {
+        console.log('post', url, data);
+
+        return new Promise((resolve, reject) => {
+            fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
+                method: 'POST',
+                body: data
+            })
+            .then(response => response.json())
+                .then((json) => {
+                    console.log('Response:', json);
+
+                    resolve(json);
+                })
+                .catch((error => {
+                    reject(error);
+                }))
+        })
     }
 }
 
